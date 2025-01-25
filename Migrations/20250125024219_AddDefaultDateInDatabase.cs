f412e64419794409908c3f45722c909b78acc84a
Benito <39175198+benitopatino@users.noreply.github.com>
2025-01-24 18:44:30 -0800

    add default date migration

---
+++ Migrations/20250125024219_AddDefaultDateInDatabase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JitAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultDateInDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DateCreated",
+                table: "Jits",
+                nullable: false,
+                defaultValueSql: "GETDATE()");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
