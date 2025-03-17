7a706bc2b32d2cec297ce1cf372ab8f6e6724ff2
Benito <39175198+benitopatino@users.noreply.github.com>
2025-03-17 00:36:55 -0700

    add relationship controller

---
+++ Controllers/RelationshipController.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using AutoMapper;
+using JitAPI.Models.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace JitAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class RelationshipController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RelationshipController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost("follow/{followeeId:guid}")]
+        public IActionResult Follow(string followeeId)
+        {
+            // extract the claims User ID 
+            var userID = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+
+            return StatusCode(200);
+        }
+
+
+    }
+}
