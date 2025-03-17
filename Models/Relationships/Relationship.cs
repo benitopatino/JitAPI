ee18a65d296e8ccab76bcac47ea5e9acd18ca356
Benito <39175198+benitopatino@users.noreply.github.com>
2025-03-16 23:17:31 -0700

    add relationship model

---
+++ Models/Relationships/Relationship.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace JitAPI.Models.Relationships
+{
+    public class Relationship
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+
+        public DateTime DateOfFollow { get; set; }
+
+        // Follower
+
+        [Required]
+        public Guid FollowerId { get; set; }
+
+        [ForeignKey("FollowerId")]
+        public User Follower { get; set; }
+
+        // Followee
+
+        [Required]
+        public Guid FolloweeId { get; set; }
+
+        [ForeignKey("FolloweeId")]
+        public User Followee { get; set; }
+    }
+}
