35034da58c5b8b2fc371abd5a81c1e8d50ae49ef
Benito <39175198+benitopatino@users.noreply.github.com>
2025-03-17 00:11:32 -0700

    add relationship migration

---
+++ Migrations/20250317070715_AddRelationships.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JitAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Relationships",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfFollow = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql:"GETDATE()"),
+                    FollowerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FolloweeId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Relationships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Relationships_Users_FolloweeId",
+                        column: x => x.FolloweeId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                    table.ForeignKey(
+                        name: "FK_Relationships_Users_FollowerId",
+                        column: x => x.FollowerId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_FolloweeId",
+                table: "Relationships",
+                column: "FolloweeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_FollowerId",
+                table: "Relationships",
+                column: "FollowerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Relationships");
+        }
+    }
+}
