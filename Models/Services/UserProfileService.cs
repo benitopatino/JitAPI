95a258abb0a3403a89b9c7ad0c5a8bc0adb90327
Benito <39175198+greatfierro@users.noreply.github.com>
2025-05-31 16:02:12 -0700

    return UserProfileDTO with new properties in UserProfileService.cs

--- Models/Services/UserProfileService.cs
+++ Models/Services/UserProfileService.cs
@@ -1,4 +1,5 @@
 using JitAPI.Models.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace JitAPI.Models.Services;
 
@@ -17,20 +18,29 @@ public class UserProfileService : IUserProfileService
     {
         if(string.IsNullOrEmpty(username))
             throw new ArgumentNullException(nameof(username));
+
+        var profile = _unitOfWork.UserProfileRepository.GetAll()
+            .Include(p => p.User) 
+            .AsNoTracking()
+            .FirstOrDefault(p => p.Username == username);
+
+        if (profile == null) return null;
         
-        var userInfo = _unitOfWork.UserRepository.GetAll()
-            .SingleOrDefault(u => u.Username == username);
-        if(userInfo == null)
-            throw new KeyNotFoundException("User not found");
-        
-        var newsfeedItems = _newsfeedService.GetNewsfeed(userInfo.UserId, true);
-       
-        return new UserProfileDTO()
+        var newsfeedItems = _newsfeedService.GetNewsfeed(profile.UserId, true);
+        return new UserProfileDTO
         {
-            FirstName = userInfo.FirstName,
-            LastName = userInfo.LastName,
-            Username = userInfo.Username,
-            NewsfeedItems = newsfeedItems.ToList()
+            FirstName = profile.User.FirstName,
+            LastName = profile.User.LastName,
+            Username = profile.Username,
+            Title = profile.Title,
+            AvatarUrl = profile.AvatarUrl,
+            Bio = profile.Bio,
+            City = profile.City,
+            StateOrProvince = profile.StateOrProvince,
+            Country = profile.Country,
+            FollowerCount = profile.FollowerCount,
+            FolloweeCount = profile.FolloweeCount,
+             NewsfeedItems = newsfeedItems.ToList()
         };
     }
 }
