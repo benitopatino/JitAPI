d0b102eb68619b92445427d8d2c3f4581221c5ba
Benito <39175198+greatfierro@users.noreply.github.com>
2025-05-30 21:13:05 -0700

    specify column of UserProfile properties

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -8,13 +8,29 @@ public class UserProfile
     [Key]
     [ForeignKey(nameof(User))] 
     public Guid UserId { get; set; }
+    
+    [Required]
+    [Column(TypeName = "varchar(250)")]
     public string Username { get; set; }
+    
+    [Column(TypeName = "varchar(250)")]
     public string Title { get; set; }
+    
+    [Column(TypeName = "varchar(500)")]
     public string AvatarUrl { get; set; }
+    
+    [Column(TypeName = "text")]
     public string Bio { get; set; }
+    
+    [Column(TypeName = "varchar(250)")]
     public string City { get; set; }
+    
+    [Column(TypeName = "varchar(250)")]
     public string StateOrProvince { get; set; }
+    
+    [Column(TypeName = "varchar(250)")]
     public string Country { get; set; }
+    
     public int FollowerCount { get; set; }
     public int FolloweeCount { get; set; }
 
