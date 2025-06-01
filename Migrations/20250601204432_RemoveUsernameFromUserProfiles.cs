c8cc6b40ae6fbca4efeff1259a67abb8aab8d24a
Benito <39175198+greatfierro@users.noreply.github.com>
2025-06-01 13:48:27 -0700

    Add migration to drop username from Userprofile. - Remove Unique constraint index - Drop Username column from UserProfiles table

---
+++ Migrations/20250601204432_RemoveUsernameFromUserProfiles.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JitAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUsernameFromUserProfiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserProfiles_Username",
+                table: "UserProfiles");
+
+            
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "UserProfiles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
