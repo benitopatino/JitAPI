96a4e4dcd35c20f706c0d97b207a4e40f9c7df7a
Benito <39175198+greatfierro@users.noreply.github.com>
2025-06-01 19:06:20 -0700

    remove unnecessary properties from CreateUserProfileDTO.cs

--- Models/DTOS/CreateUserProfileDTO.cs
+++ Models/DTOS/CreateUserProfileDTO.cs
@@ -3,9 +3,6 @@ namespace JitAPI.Models.DTOS;
 public class CreateUserProfileDTO
 {
     public Guid UserId { get; set; }
-    public string? Title { get; set; }
-    public string? AvatarUrl { get; set; }
-    public string? Bio { get; set; }
     public string? City { get; set; }
     public string? StateOrProvince { get; set; }
     public string? Country { get; set; }
