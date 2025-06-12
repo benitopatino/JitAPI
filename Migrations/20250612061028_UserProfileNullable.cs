3c3558d5855f42348804555a50c964d02eabc576
Benito <39175198+benitopatino@users.noreply.github.com>
2025-06-11 23:11:33 -0700

    Add migration for UserProfileNullbale

---
+++ Migrations/20250612061028_UserProfileNullable.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JitAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserProfileNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StateOrProvince",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "UserProfiles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvatarUrl",
+                table: "UserProfiles",
+                type: "varchar(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StateOrProvince",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "UserProfiles",
+                type: "varchar(250)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "UserProfiles",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvatarUrl",
+                table: "UserProfiles",
+                type: "varchar(500)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldNullable: true);
+        }
+    }
+}
