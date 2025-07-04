7223515a7ce89b559ae69f17acbc819a7725e268
Benito <39175198+benitopatino@users.noreply.github.com>
2025-07-04 15:38:06 -0700

    get list of followees username

--- Models/Interface/IUserProfileService.cs
+++ Models/Interface/IUserProfileService.cs
@@ -7,6 +7,12 @@ public interface IUserProfileService
 {
     UserProfileDTO GetUserProfile(string username);
     UserProfileDTO GetUserProfile(Guid userId);
+    /// <summary>
+    /// Return a collection of usernames belonging to followees.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    IEnumerable<string> GetFollowees(Guid userId);
     void UpdateFollowersCount(Guid userId, UpdateAction action);
     void UpdateFolloweeCount(Guid userId, UpdateAction action);
     void CreateUserProfile(CreateUserProfileDTO newUserProfile);
