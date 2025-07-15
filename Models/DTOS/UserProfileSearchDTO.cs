16489dffc016467c988597ea7323478bf4e67da2
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2025-07-14 21:29:03 -0700

    Create UserProfileSearchDTO.cs

---
+++ Models/DTOS/UserProfileSearchDTO.cs
@@ -0,0 +1,9 @@
+namespace JitAPI.Models.DTOS;
+
+public class UserProfileSearchDTO
+{
+    public string AvatarUrl { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+}
