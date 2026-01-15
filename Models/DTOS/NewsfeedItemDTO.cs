5e9a4b2e7b3a555316157426aff7f0fcb5fe21d1
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2026-01-15 12:01:56 -0800

    add AvatarUrl property to NewsfeedItemDTO

--- Models/DTOS/NewsfeedItemDTO.cs
+++ Models/DTOS/NewsfeedItemDTO.cs
@@ -5,11 +5,13 @@ public class NewsfeedItemDTO(
     string jitContent,
     DateTime jitDateCreated,
     Guid authorUserId,
-    string authorFullName)
+    string authorFullName,
+    string avatarUrl)
 {
     public Guid Id { get; set; } = jitId;
     public string Content { get; set; } = jitContent;
     public DateTime DateCreated { get; set; } = jitDateCreated;
     public Guid AuthorUserId { get; set; } = authorUserId;
     public string AuthorFullName { get; set; } = authorFullName;
+    public string AvatarUrl { get; set; } = avatarUrl;
 }
